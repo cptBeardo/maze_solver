6badbaf4f7987cc6a1248524ce13acc02ad0e7d7
cptBeardo <cam.mic.sag@gmail.com>
2025-04-04 09:27:09 -0500

    Added Point() and Line() classes and successfully ran the main.py to draw the lines

--- creation.py
+++ creation.py
@@ -23,3 +23,25 @@ class Window():
     
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color="black"):
+        x1 = self.point1.x
+        y1 = self.point1.y
+        x2 = self.point2.x
+        y2 = self.point2.y
+
+        canvas.create_line(x1, y1, x2, y2, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from creation import Window
+from creation import Window, Point, Line
 
 
 def main():
     win = Window(800, 800)
+
+    """this is for testing"""
+    p1 = Point(100, 100)
+    p2 = Point(700, 100)
+    p3 = Point(100, 500)
+    p4 = Point(700, 500)
+
+    line1 = Line(p1, p2)
+    line2 = Line(p3, p4)
+    line3 = Line(p1, p3)
+    line4 = Line(p2, p4)
+    line5 = Line(p1, p4)
+
+    win.draw_line(line1, "red")
+    win.draw_line(line2, "blue")
+    win.draw_line(line3, "green")
+    win.draw_line(line4, "purple")
+    win.draw_line(line5, "orange")
+
+    """end of the test lines"""
+
+
     win.wait_for_close()
 
 
